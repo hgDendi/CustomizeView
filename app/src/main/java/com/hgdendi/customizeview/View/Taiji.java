66ca4edffce309fa206ef0acaf6caef4d77c446d
hgDendi <hg.dendi@gmail.com>
2016-12-18 13:58:16 +0800

    use picture to optimize drawing

--- app/src/main/java/com/hgdendi/customizeview/View/Taiji.java
+++ app/src/main/java/com/hgdendi/customizeview/View/Taiji.java
@@ -5,11 +5,12 @@ import android.graphics.Canvas;
 import android.graphics.Paint;
 import android.graphics.Picture;
 import android.graphics.RectF;
+import android.support.v4.content.ContextCompat;
 import android.util.AttributeSet;
 import android.view.View;
 
 /**
- * Created by dendich on 17/12/2016.
+ * Created by hgDendi on 17/12/2016.
  */
 
 public class Taiji extends View {
@@ -38,12 +39,12 @@ public class Taiji extends View {
 
     private void init() {
         mBlackPaint = new Paint();
-        mBlackPaint.setColor(getResources().getColor(android.R.color.black));
+        mBlackPaint.setColor(ContextCompat.getColor(getContext(), android.R.color.black));
         mBlackPaint.setStyle(Paint.Style.FILL);
 
 
         mWhitePaint = new Paint();
-        mWhitePaint.setColor(getResources().getColor(android.R.color.white));
+        mWhitePaint.setColor(ContextCompat.getColor(getContext(), android.R.color.white));
         mWhitePaint.setStyle(Paint.Style.FILL);
 
         mRotateDegrees = 0;
@@ -59,19 +60,10 @@ public class Taiji extends View {
     }
 
     private void initPicture() {
-        mPicture.beginRecording(mWidth, mHeight);
-    }
+        Canvas canvas = mPicture.beginRecording(mWidth, mHeight);
+        int radius = Math.min(mWidth, mHeight) / 2;
 
-    @Override
-    public void draw(Canvas canvas) {
-        super.draw(canvas);
-        float radius;
-        int width = getWidth();
-        int height = getHeight();
-        radius = Math.min(width, height) / 2;
-
-        canvas.translate(width / 2, height / 2);
-        canvas.rotate(mRotateDegrees);
+        canvas.translate(mWidth / 2, mHeight / 2);
         RectF circleRect = new RectF(-radius, -radius, radius, radius);
         canvas.drawArc(circleRect, 270, 180, true, mBlackPaint);
         canvas.drawArc(circleRect, 90, 180, true, mWhitePaint);
@@ -86,6 +78,15 @@ public class Taiji extends View {
         canvas.drawCircle(0, 0, radius / 2, mWhitePaint);
         canvas.drawCircle(0, 0, radius / 8, mBlackPaint);
         canvas.restore();
+        mPicture.endRecording();
+    }
+
+    @Override
+    public void draw(Canvas canvas) {
+        super.draw(canvas);
+
+        canvas.rotate(mRotateDegrees, mWidth / 2, mHeight / 2);
+        canvas.drawPicture(mPicture);
 
         mRotateDegrees += 5;
         invalidate();
