368b3e6b779e6526f51e0a4a7715a65ec1b29b1f
hgDendi <hg.dendi@gmail.com>
2016-12-25 16:11:37 +0800

    sovle ACTION_MOVE conflicts

--- app/src/main/java/com/hgdendi/customizeview/View/PathTestView.java
+++ app/src/main/java/com/hgdendi/customizeview/View/PathTestView.java
@@ -5,11 +5,13 @@ import android.graphics.Canvas;
 import android.graphics.Paint;
 import android.graphics.Path;
 import android.graphics.Point;
-import android.support.annotation.NonNull;
 import android.support.v4.content.ContextCompat;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
+
+import static android.content.ContentValues.TAG;
 
 /**
  * Created by hg.dendi on 18/12/2016.
@@ -20,6 +22,7 @@ public class PathTestView extends View {
     private int mHeight;
     private Paint mPaint;
     private Point controlPoint;
+    private Path mPath;
 
     public PathTestView(Context context) {
         super(context);
@@ -46,6 +49,19 @@ public class PathTestView extends View {
     @Override
     public boolean onTouchEvent(MotionEvent event) {
 
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_CANCEL:
+                Log.d(TAG, "onTouchEvent: cancel");
+                return false;
+            case MotionEvent.ACTION_UP:
+                getParent().requestDisallowInterceptTouchEvent(false);
+                break;
+            case MotionEvent.ACTION_DOWN:
+                getParent().requestDisallowInterceptTouchEvent(true);
+                break;
+        }
+
+
         controlPoint.x = (int) event.getX();
         controlPoint.y = (int) event.getY();
         invalidate();
@@ -56,11 +72,11 @@ public class PathTestView extends View {
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
 
-        Path path = new Path();
-        path.moveTo(0, 0);
-        path.quadTo(controlPoint.x, controlPoint.y, mWidth, mHeight);
+        mPath.reset();
+        mPath.moveTo(0, mHeight / 2);
+        mPath.quadTo(controlPoint.x, controlPoint.y, mWidth, mHeight / 2);
 
-        canvas.drawPath(path, mPaint);
+        canvas.drawPath(mPath, mPaint);
 
     }
 
@@ -69,6 +85,8 @@ public class PathTestView extends View {
         mPaint.setStyle(Paint.Style.STROKE);
         mPaint.setColor(ContextCompat.getColor(getContext(), android.R.color.black));
         mPaint.setStrokeWidth(10);
+
+        mPath = new Path();
 
         controlPoint = new Point(0, 0);
     }
